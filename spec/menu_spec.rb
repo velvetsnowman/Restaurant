89b5f02c6ce2b45079d2ff6e93c31b6d932a1236
Daniel den Hartog <daniel.mista@gmail.com>
2018-09-17 20:20:37 +0100

    no tests, done for now

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,6 +1,18 @@
 require 'menu'
 require 'dish'
+require 'order'
+require 'confirmation.rb'
+
 describe Menu do
   let (:dish) { Dish.new }
   let (:menu) { Menu.new }
+
+  it 'should let an owner add a dish to the menu' do
+    menu.add_to_menu("lamb", 3)
+    expect(menu.menu_items.length).to eq 1
+  end
+
+  it "should let a user order" do
+    
+  end
 end
